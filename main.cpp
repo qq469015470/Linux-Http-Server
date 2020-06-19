8ccc44ee79404cc54427b246d7297d56c5171588
Ubuntu 64位 <qq469015470@outlook.com>
2020-06-19 02:57:28 -0700

    使用epoll测试接受数据

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <assert.h>
 #include <sys/socket.h>
+#include <sys/epoll.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdexcept>
@@ -21,27 +22,102 @@ void ServerProc(sockaddr_in _sockAddr)
 
 	bind(serverSock, reinterpret_cast<sockaddr*>(&_sockAddr), sizeof(_sockAddr));
 
-	listen(serverSock, 20);
+	const int max = 20;
+
+	listen(serverSock, max);
 
 	std::cout << "server start listen..." << std::endl;
 
+	epoll_event ev;
+	epoll_event events[max];
+	const int epfd = epoll_create(max);
+
+	if(epfd == -1)
+	{
+		std::cout << "create epoll faile!" << std::endl;
+		return;
+	}
+
+	ev.data.fd = serverSock;
+	ev.events = EPOLLIN;
+	if(epoll_ctl(epfd, EPOLL_CTL_ADD, serverSock, &ev) == -1)
+	{
+		std::cout << "epoll control failed!" << std::endl;
+		return;
+	}
+
 	while(true)
 	{
-		sockaddr_in clntAddr = {};
-		socklen_t size = sizeof(clntAddr);
+		int nfds = epoll_wait(epfd, events, max, -1);
 
-		int clientSock = accept(serverSock, reinterpret_cast<sockaddr*>(&clntAddr), &size);
-		char content[256];
+		if(nfds == -1)
+		{
+			std::cout << "epoll wait failed!" << std::endl;
+		}
 
-		memset(content, 0, sizeof(content));
+		std::cout << "nfds:" << nfds  << std::endl;
 
-		recv(clientSock, content, sizeof(content), 0);
+		for(int i = 0; i < nfds; i++)
+		{
+			if(events[i].data.fd == serverSock)
+			{
+				sockaddr_in clntAddr = {};
+		             	socklen_t size = sizeof(clntAddr);
 
-		std::cout << "server recv:" << content << std::endl;
+				const int connfd = accept(serverSock, reinterpret_cast<sockaddr*>(&clntAddr), &size);
+				if(connfd == -1)
+				{
+					std::cout << "accept failed" << std::endl;
+					return;
+				}
 
-		strcpy(content, "server reply!");
+				ev.data.fd = connfd;
+				ev.events = EPOLLIN;
+				epoll_ctl(epfd, EPOLL_CTL_ADD, connfd, &ev);
+				std::cout << "accept client_addr" << inet_ntoa(clntAddr.sin_addr) << std::endl;
 
-		send(clientSock, content, sizeof(content), 0);
+			}
+			else if(events[i].events & EPOLLIN)
+			{
+				char buffer[256];
+
+				memset(buffer, 0, sizeof(buffer));
+				const int ret =	recv(events[i].data.fd, buffer, sizeof(buffer), 0);
+
+				strcpy(buffer, "server reply!");
+				send(events[i].data.fd, buffer, sizeof(buffer), 0);
+
+				if(ret <= 0)
+				{
+					close(events[i].data.fd);
+					epoll_ctl(epfd, EPOLL_CTL_DEL, events[i].data.fd, &events[i]);
+				}
+				else
+				{
+					std::cout << "server recv:" << buffer << std::endl;
+				}
+			}
+			else
+			{
+				std::cout << "something else happen" << std::endl;
+			}
+		}
+
+//		sockaddr_in clntAddr = {};
+//		socklen_t size = sizeof(clntAddr);
+//
+//		int clientSock = accept(serverSock, reinterpret_cast<sockaddr*>(&clntAddr), &size);
+//		char content[256];
+//
+//		memset(content, 0, sizeof(content));
+//
+//		recv(clientSock, content, sizeof(content), 0);
+//
+//		std::cout << "server recv:" << content << std::endl;
+//
+//		strcpy(content, "server reply!");
+//
+//		send(clientSock, content, sizeof(content), 0);
 	}
 
 	close(serverSock);
@@ -87,14 +163,14 @@ int main()
 	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	serverAddr.sin_port = htons(8888);
 
-//	ServerProc(serverAddr);
-
-//	return 0;
-
 	std::thread serverProc(ServerProc, serverAddr);
 
 	for(int i = 0; i < 100; i++)
-		ClientProc(serverAddr);
+	{
+		std::thread proc(ClientProc, serverAddr);
+
+		proc.detach();
+	}
 
 	serverProc.join();
 
