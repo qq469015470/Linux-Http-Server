a4f55c1313bd8fc7cd03628357e8eb5c3ac1491e
qq469015470 <469015470@qq.com>
2021-09-15 13:41:35 +0800

    rename warehouse table

--- src/WareHouseService.hpp
+++ src/WareHouseService.hpp
@@ -18,7 +18,7 @@ private:
 public:
 	std::vector<WareHouse> GetWareHouses()
 	{
-		auto dataTable = this->mysqlSubmitService.Query("select * from WareHouse");
+		auto dataTable = this->mysqlSubmitService.Query("select * from wareHouse");
 
 		std::vector<WareHouse> result;
 		for(const auto& item: dataTable)
@@ -31,10 +31,10 @@ public:
 
 	void AddWareHouse(std::string_view _name)
 	{
-		auto dataTable = this->mysqlSubmitService.Query("select 1 from WareHouse where name = ?", _name);
+		auto dataTable = this->mysqlSubmitService.Query("select 1 from wareHouse where name = ?", _name);
 		if(dataTable.size() > 0)
 			throw std::logic_error("仓库名称不能重复!");
 
-		this->mysqlSubmitService.ExecuteCommand("insert into WareHouse(name) values(?)", _name);
+		this->mysqlSubmitService.ExecuteCommand("insert into wareHouse(name) values(?)", _name);
 	}
 };
